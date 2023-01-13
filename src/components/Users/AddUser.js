ef6449ac7a1107f94fa9b5e6744776b2d542061d
pjevic <milanpjevic@icloud.com>
2023-01-13 13:47:33 +0100

    Adding User and Card component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="text"></input>
+
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
